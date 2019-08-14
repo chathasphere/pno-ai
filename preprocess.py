3214948ebaa08fe1fe06f02d4cc8430b486ea910
Nikku Chatha <pgchatha@gmail.com>
2019-08-14 18:02:02 -0400

    added split sampling, time stretching to midi pipeline

--- preprocess.py
+++ preprocess.py
@@ -2,15 +2,25 @@ import os
 import pretty_midi
 import six
 import copy
+import pdb
 
 class PreprocessingError(Exception):
     pass
 
 class PreprocessingPipeline():
 
-    def __init__(self, input_dir):
+    def __init__(self, input_dir, stretch_factors = [0.95, 0.975, 1, 1.025, 1.05],
+            split_size = 30, sampling_rate = 125):
         self.input_dir = input_dir
         self.note_sequences = []
+        #size (in seconds) in which to split midi samples
+        self.split_samples = []
+        self.stretch_factors = stretch_factors
+        self.split_size = split_size
+        self.quantized_samples = []
+        #In hertz (beats per second), quantize samples to this discrete frequency
+        #So a sampling rate of 125 hz means a smallest time steps of 8 ms
+        self.sampling_rate = sampling_rate
 
 
     def run(self):
@@ -22,10 +32,33 @@ class PreprocessingPipeline():
         print("{} midis read, or {:.1f} minutes of music"\
                 .format(len(midis), total_time))
 
+        self.note_sequences = self.get_note_sequences(midis)
+        self.note_sequences = self.stretch_note_sequences()
+        #stretch the note sequences?
+        self.split_sequences()
 
-        self.get_note_sequences(midis)
+    def convert_files(self):
+        pretty_midis = []
+        folders = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+        if len(folders) > 0:
+            for d in folders:
+                os.chdir(d)
+                pretty_midis += convert_files()
+                os.chdir("..")
+        midis = [f for f in os.listdir(os.getcwd()) if \
+                (f.endswith(".mid") or f.endswith("midi"))]
+        for m in midis:
+            with open(m, "rb") as f:
+                try:
+                    midi_str = six.BytesIO(f.read())
+                    pretty_midis.append(pretty_midi.PrettyMIDI(midi_str))
+                    print("Successfully parsed {}!".format(m))
+                except:
+                    print("Could not parse {}".format(m))
+        return pretty_midis
 
     def get_note_sequences(self, midis):
+        note_sequences = []
         for m in midis:
             if m.instruments[0].program == 0:
                 piano_data = m.instruments[0]
@@ -33,13 +66,16 @@ class PreprocessingPipeline():
                 #todo: write logic to safely catch if there are non piano instruments,
                 #or extract the piano midi if it exists
                 raise PreprocessingError("Non-piano midi detected")
-            self.note_sequences.append(self.apply_sustain(piano_data))
-            
+            note_sequences.append(self.apply_sustain(piano_data))
+
+        return note_sequences
+
 
     def apply_sustain(self, piano_data):
         """
-        While the sustain pedal is applied during a midi, extend the length of all notes to the beginning of the next note of the same pitch or to the end of the sustain. Returns a
-        midi notes sequence.
+        While the sustain pedal is applied during a midi, extend the length of all 
+        notes to the beginning of the next note of the same pitch or to 
+        the end of the sustain. Returns a midi notes sequence.
         """
         _SUSTAIN_ON = 0
         _SUSTAIN_OFF = 1
@@ -111,23 +147,47 @@ class PreprocessingPipeline():
                     live_notes.remove(note)
         return notes
 
-    def convert_files(self):
-        pretty_midis = []
-        folders = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
-        if len(folders) > 0:
-            for d in folders:
-                os.chdir(d)
-                pretty_midis += convert_files()
-                os.chdir("..")
-        midis = [f for f in os.listdir(os.getcwd()) if \
-                (f.endswith(".mid") or f.endswith("midi"))]
-        for m in midis:
-            with open(m, "rb") as f:
-                try:
-                    midi_str = six.BytesIO(f.read())
-                    pretty_midis.append(pretty_midi.PrettyMIDI(midi_str))
-                    print("Successfully parsed {}!".format(m))
-                except:
-                    print("Could not parse {}".format(m))
-        return pretty_midis
+    def stretch_note_sequences(self):
+        stretched_note_sequences = []
+        for note_sequence in self.note_sequences:
+            for factor in self.stretch_factors:
+                if factor == 1:
+                    stretched_note_sequences.append(note_sequence)
+                    continue
+                stretched_sequence = copy.deepcopy(note_sequence)
+                for note in stretched_sequence:
+                    note.start *= factor
+                    note.end *= factor
+                stretched_note_sequences.append(stretched_sequence)
+
+        return stretched_note_sequences
+    
+
+    def split_sequences(self):
+        if len(self.note_sequences) == 0:
+            raise PreprocessingError("No note sequences available to split")
+
+        for note_sequence in self.note_sequences:
+            sample_length = 0
+            sample = []
+            i = 0
+            while i < len(note_sequence):
+                note = note_sequence[i]
+                if sample_length == 0:
+                    sample_start = note.start
+                new_length = note.end - sample_start
+                if new_length <= self.split_size:
+                    sample.append(note)
+                    sample_length = new_length
+                else:
+                    if len(sample) == 0:
+                        raise PreprocessingError("Sample could not be split!")
+                    self.split_samples.append(sample)
+                    #sample start should begin with the beginning of the
+                    #*next* note, how do I handle this...
+                    sample_length = 0
+                    sample = []
+                i += 1
+
+
 
--- preprocess_test.py
+++ preprocess_test.py
@@ -14,11 +14,28 @@ def write_to_midi(note_sequences, output_dir):
         i += 1
         #with open(output_name, 'w')
         midi.write(output_name)
+    print("Piano data successfully extracted from midis, navigate to {} to listen"\
+            .format(output_dir))
+
+def check_sample_lengths(split_samples, split_size):
+    for i in range(len(split_samples)):
+        sample = split_samples[i]
+        sample_end = sample[-1].end
+        sample_start = sample[0].start
+        try:
+            assert sample_end - sample_start <= split_size
+        except AssertionError:
+            print("Error with sample {}; start is {.2f} and end is {.2f}"\
+                    .format(i, sample_end, sample_start))
+    print("All samples are less than {} seconds in length.".format(split_size))
 
 def main():
-    pipeline = PreprocessingPipeline(input_dir = "data/test")
+    pipeline = PreprocessingPipeline(input_dir = "data/test", split_size = 30)
     pipeline.run()
-    write_to_midi(pipeline.note_sequences, "output/test")
+    # write_to_midi(pipeline.note_sequences, "output/test")
+    check_sample_lengths(pipeline.split_samples, 30)
+    # pdb.set_trace()
+
 
 if __name__ == "__main__":
     main()
