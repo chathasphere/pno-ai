af13312a03d376da5c3b2c37196516febe8a6343
Nikku Chatha <pgchatha@gmail.com>
2019-08-20 10:49:42 -0400

    debugging sequence encoder

--- sequence_encoder.py
+++ sequence_encoder.py
@@ -1,3 +1,6 @@
+from pretty_midi import Note
+import pdb
+
 class SequenceEncoderError(Exception):
     pass
 
@@ -15,18 +18,13 @@ class SequenceEncoder():
     Includes functions to cast a sequence of Midi Notes to a numeric list of 
     possible events and to one-hot encode a numeric sequence as a Pytorch tensor.
     """
-    action_ordering = {
-            "VELOCITY": 0,
-            "NOTE_OFF": 1,
-            "NOTE_ON": 2
-            }
 
     def __init__(self, n_time_shift_events, n_velocity_events,
             sequences_per_update=100):
         self.n_time_shift_events = n_time_shift_events
-        self.n_velocity_events = n_velocity_events
         self.n_events = 256 + n_time_shift_events + n_velocity_events
         self.timestep = 1 / n_time_shift_events
+        self.velocity_bin_size = 128 // n_velocity_events
         self.sequences_per_update = sequences_per_update
 
     def encode_event_sequences(self, sample_sequences):
@@ -59,18 +57,16 @@ class SequenceEncoder():
                 event_timestamps.append((t0, "NOTE_ON", p))
                 event_timestamps.append((t1, "NOTE_OFF", p))
 
-            # sort events by timestamp and action
-            event_timestamps = sorted(event_timestamps, 
-                    key = lambda x: (x[0], SequenceEncoder.action_ordering[x[1]]))
-
+            # sort events by timestamp
+            event_timestamps = sorted(event_timestamps, key = lambda x: x[0])
             current_time = 0
             max_timeshift = self.n_time_shift_events
+            #this loop encodes timeshifts as numbers
             for timestamp in event_timestamps:
                 #capture a timeshift
                 if timestamp[0] != current_time:
-                    #I feel cautious about this casting...
-                    timeshift = int((timestamp[0] / self.timestep) - \
-                            (current_time / self.timestep))
+                    timeshift = (timestamp[0] *  self.n_time_shift_events) - \
+                            (current_time * self.n_time_shift_events)
                     timeshift_events = []
                     #aggregate pauses longer than one second, as necessary
                     while timeshift > max_timeshift:
@@ -84,10 +80,9 @@ class SequenceEncoder():
                     event_sequence.extend(timeshift_events)
                     
                     #add the other events: NOTE_ON, NOTE_OFF, VELOCITY
-                    event_sequence.append(
-                            self.event_to_number(timestamp[1], timestamp[2]))
                     current_time = timestamp[0]
-
+                event_sequence.append(
+                        self.event_to_number(timestamp[1], timestamp[2]))
             event_sequences.append(event_sequence)
 
         return event_sequences
@@ -102,10 +97,87 @@ class SequenceEncoder():
         elif event == "NOTE_OFF":
             return value + 128
         elif event == "TIME_SHIFT":
-            return value + 256
+            #subtract one to fit to zero-index convention
+            #i.e. the number 256 corresponds to the smallest possible timestep
+            #which is non-zero...!
+            return value + 256 - 1
         elif event == "VELOCITY":
-            return value + 256 + self.n_time_shift_events
+            #convert to bins
+            v_bin = (value - 1) // self.velocity_bin_size
+            return v_bin + 256 + self.n_time_shift_events
         else:
             raise SequenceEncoderError("Event type {} not recognized".format(event))
+
+    def number_to_event(self, number):
+        number = int(number)
+        if number < 0 or number >= self.n_events:
+            raise SequenceEncoderError("Number {} out of range")
+
+        if number < 128:
+            event = "NOTE_ON", number
+        elif 128 <= number < 256:
+            event = "NOTE_OFF", number - 128
+        elif 256 <= number < 256 + self.n_time_shift_events:
+            event = "TIME_SHIFT", number + 1 - 256
+        else:
+            bin_number = number - 256 - self.n_time_shift_events
+            event = "VELOCITY", (bin_number * self.velocity_bin_size) + 1
+
+        return event
+
+
+    def decode_event_sequences(self, encoded_sequences):
+        note_sequences = []
+        for encoded_sequence in encoded_sequences:
+            events = []
+            for num in encoded_sequence:
+                events.append(self.number_to_event(num))
+            #WARNING DANGER
+            #data (timestamps) already corrupted by this point
+            #list of pseudonotes = {'start':x, 'pitch':something, 'velocity':something}
+            notes = []
+            #on the second pass, add in end time
+            note_ons = []
+            note_offs = []
+            global_time = 0
+            current_velocity = 0
+            for event, value in events:
+                #check event type
+                if event == "TIME_SHIFT":
+                    global_time += 0.008 * value
+                    global_time = round(global_time, 5)
+
+                elif event == "VELOCITY":
+                    current_velocity = value
+                
+                elif event == "NOTE_OFF":
+                    #eventually we'll sort this by timestamp and work thru
+                    note_offs.append({"pitch": value, "end": global_time})
+                
+                elif event == "NOTE_ON":
+                    #it's a NOTE_ON!
+                    #value is pitch 
+                    note_ons.append({"start": global_time, 
+                        "pitch": value, "velocity": current_velocity})
+                else:
+                    raise SequenceEncoderError("you fool!")
+            #second pass: zip up notes with their note-offs
+            while len(note_ons) > 0:
+                note_on = note_ons[0]
+                pitch = note_on['pitch']
+                #this assumes everything is sorted nicely!
+                note_off = next(n for n in note_offs if n['pitch'] == pitch)
+                note = Note(start = note_on['start'], end = note_off['end'],
+                        pitch = pitch, velocity = note_on['velocity'])
+                notes.append(note)
+                note_ons.remove(note_on)
+                note_offs.remove(note_off)
+            
+            note_sequences.append(notes)
+
+        return note_sequences
+
+            
+
 
 
--- sequence_encoder_test.py
+++ sequence_encoder_test.py
@@ -27,11 +27,37 @@ sample_note_sequence = [
     Note(start=4.8, end=5.8, pitch=40, velocity=37)]
 ]
 
+
+sample_note_sequence1 = [
+    [Note(start=0.928000, end=1.720000, pitch=54, velocity=25), 
+    Note(start=0.952000, end=1.744000, pitch=42, velocity=25), 
+    Note(start=0.952000, end=1.720000, pitch=47, velocity=29), 
+    Note(start=1.384000, end=1.944000, pitch=62, velocity=41), 
+    Note(start=1.384000, end=1.968000, pitch=59, velocity=29)]
+        ]
+
 def main():
     encoder = SequenceEncoder(n_time_shift_events = 125, n_velocity_events = 32)
     assert encoder.n_events == 413
     encoded = encoder.encode_event_sequences(sample_note_sequence)
-    pdb.set_trace()
+    decoded = encoder.decode_event_sequences(encoded)
+    
+    original_seq = sorted(sample_note_sequence[0], key = lambda x: x.start)
+    decoded_seq = sorted(decoded[0], key = lambda x: x.start)
+    for o,d in zip(original_seq, decoded_seq):
+        try:
+            assert o.start == d.start
+            assert o.end == d.end
+            assert o.pitch == d.pitch
+            assert o.velocity == d.velocity
+        except AssertionError:
+            print("Encoding/Decoding error detected!")
+            print("Original note:")
+            print(o)
+            print("Decoded encoded note:")
+            print(d)
+            print('************')
+
 
 if __name__ == "__main__":
     main()
