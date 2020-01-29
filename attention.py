Fix: subsequent masking bug

attention.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import math
 import torch.nn.functional as F
+from helpers import d
 
 class AttentionError(Exception):
     pass
@@ -21,7 +22,7 @@ class MultiheadedAttention(nn.Module):
         self.heads = heads
         s = d_model // heads
         self.linears = torch.nn.ModuleList([nn.Linear(s, s, bias=False) for i in range(3)])
-        self.unifyheads = nn.Linear(heads * s, d_model)
+        self.recombine_heads = nn.Linear(heads * s, d_model)
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x, mask):
@@ -36,21 +37,16 @@ class MultiheadedAttention(nn.Module):
         queries, keys, values = [linear(x).transpose(1,2).\
                                  contiguous().view(b*h, t, s)
           for linear, x in zip(self.linears, (x,x,x))]
-
         #Compute scaled dot-product self-attention
-        #scale pre-matrix multiplication        
+        #scale pre-matrix multiplication   
         queries = queries / (e ** (1/4))
         keys    = keys / (e ** (1/4))
 
-        #Compatibility scores of queries and keys via dot product
-        scores = torch.bmm(queries, keys.transpose(1, 2))
-
         # mask out the upper half of the dot matrix, excluding the diagonal
-        if mask is not None:
-            #repeat mask per head...
-            mask = torch.repeat_interleave(mask, h, 0)
-            scores = scores.masked_fill(mask == 0, -1e9)
-
+        scores = torch.bmm(queries, keys.transpose(1, 2))
+        subsequent_mask = torch.triu(torch.ones(1, t, t, device=d()), 0)
+        scores = scores.masked_fill(subsequent_mask.transpose(1,2) == 0, -1e9)
+        
         #Convert scores to probabilities
         attn_probs = F.softmax(scores, dim=2)
         attn_probs = self.dropout(attn_probs)
@@ -59,5 +55,4 @@ class MultiheadedAttention(nn.Module):
         #transpose and recombine attention heads
         out = out.transpose(1, 2).contiguous().view(b, t, s * h)
         #last linear layer of weights
-        return self.unifyheads(out)
-
+        return self.recombine_heads(out)